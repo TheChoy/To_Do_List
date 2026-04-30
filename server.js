1e35fb481874865493d2a0bf38d447bfd0999932
TheChoy <pay_26448@hotmail.com>
2026-05-01 02:29:45 +0700

    full rest api

--- server.js
+++ server.js
@@ -1,21 +1,80 @@
 const express = require("express")
 const app = express()
-const bodyparser = require('body-parser') 
+
+const bodyparser = require('body-parser')
+
 const port = 3000
-app.use(bodyparser.text())
+
+app.use(bodyparser.json())
 
 let todos = []
+let counter = 1
 
 app.get("/getTodo", function (req, res) {
     res.json(todos)
 })
 
-app.post("/inputTodo", function(req, res){
+app.post("/inputTodo", function (req, res) {
     let todo = req.body
+    todo.id = counter
+    counter += 1
     todos.push(todo)
     res.json({
-        massage : "Add complete",
-        Todo : todos
+        massage: "Add complete",
+        Todo: todos
+    })
+})
+
+app.put('/user/:id', function (req, res) {
+    let id = req.params.id
+    let updateTodo = req.body
+
+    let selectedIndex = todos.findIndex(todo => todo.id == id)
+
+    todos[selectedIndex].firsname = updateTodo.firsname || todos[selectedIndex].firsname
+    todos[selectedIndex].lastname = updateTodo.lastname || todos[selectedIndex].lastname
+
+    res.json({
+        massage: 'update conplete',
+        data: {
+            todo: updateTodo,
+            indexUpdate: selectedIndex
+        }
+    })
+})
+
+app.patch('/user/:id', function (req, res) {
+    let id = req.params.id
+    let updateTodo = req.body
+
+    let selectedIndex = todos.findIndex(todo => todo.id == id)
+
+    if (updateTodo.firsname) {
+        todos[selectedIndex].firsname = updateTodo.firsname
+    }
+    if (updateTodo.lastname) {
+        todos[selectedIndex].lastname = updateTodo.lastname
+    }
+
+    res.json({
+        massage: 'update conplete',
+        data: {
+            todo: updateTodo,
+            indexUpdate: selectedIndex
+        }
+    })
+})
+
+app.delete('/user/:id', function (req, res) {
+    let id = req.params.id
+
+    let selectedIndex = todos.findIndex(todo => todo.id == id)
+
+    todos.splice(selectedIndex, 1)
+
+    res.json({
+        massage : 'deleted',
+        indexDeleted : selectedIndex
     })
 })
 
