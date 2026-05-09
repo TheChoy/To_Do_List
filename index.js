201dc87ed5475174c24d4df23f5eb6d6be38f19d
TheChoy <pay_26448@hotmail.com>
2026-05-09 21:12:09 +0700

    เพิ่ม function clock

--- index.js
+++ index.js
@@ -1,39 +1,89 @@
 let ul = document.getElementById("ul")
 let inputTodo = document.getElementById("textTodo")
 let btnsubmit = document.getElementById("submitbutton")
-btnsubmit.addEventListener("click", function () {
 
-    let newItem = document.createElement("li")
-    let text = document.createElement("span")
-    let edite = document.createElement("button")
-    let deletebtn = document.createElement("button")
+let tasks = [];
+let editid = null;
 
-    edite.textContent = "edite"
-    deletebtn.textContent = "delete"
-    newItem.textContent = inputTodo.value
-
-    newItem.appendChild(text)
-    newItem.appendChild(edite)
-    newItem.appendChild(deletebtn)
-
-    edite.addEventListener("click", function () {
-        let newText = prompt("edite", text.textContent)
-        if (newText !== null && newText.trim() !== "") {
-            newItem.textContent = newText
+btnsubmit.addEventListener("click", () => {
+    const text = inputTodo.value.trim();
+    if (text === "") return;
+    if(editid !== null){
+    tasks = tasks.map(task =>{
+        if ( task.id === editid){
+            task.title = text;
         }
-        newItem.appendChild(edite)
-        newItem.appendChild(deletebtn)
+        return task;
     })
-
-    deletebtn.addEventListener("click", function () {
-        newItem.remove()
-    })
-
-    ul.appendChild(newItem)
-    inputTodo.value = ""
-})
-inputTodo.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-        btnsubmit.click()
+    editid = null;
+    btnsubmit.textContent = "Add";
+    } else{   
+        const task = {
+            id : Date.now(),
+            title : text,
+            complete : false
+        }
+        tasks.push(task);
     }
+    inputTodo.value = "";
+    renderTasks();
 })
+
+function editTask(id){
+    const task = tasks.find(task => task.id ===id);
+    inputTodo.value = task.title;
+    editid = id;
+    btnsubmit.textContent = "Update";
+}
+
+function renderTasks(){
+    ul.innerHTML = "";
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+        <span style="${task.complete ? 'text-decoration: line-through;' : ''}">
+            ${task.title}
+        </span>
+        <button onclick="editTask(${task.id})"> Edit </button>
+        <button onclick="toggleTask(${task.id})"> Complete </button>
+        <button onclick="deleteTask(${task.id})"> Delete </button>        
+        `;
+
+        ul.appendChild(li);
+    });
+}
+
+function deleteTask(id){
+    tasks = tasks.filter(task => task.id !== id);
+    renderTasks();
+}
+
+//clock
+const clock = document.getElementById("clock");
+const dateEl = document.getElementById("date");
+
+function updateClock() {
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    hours = String(hours).padStart(2,"0");
+    minutes = String(minutes).padStart(2,"0");
+    seconds = String(seconds).padStart(2,"0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds} ${ampm}`;
+
+    const options = {weekday:"long",year:"numeric",month:"long",day:"numeric"};
+    dateEl.innerText = now.toLocaleDateString(undefined,options)
+}
+
+setInterval(updateClock,1000);
+updateClock();
